ace2fbe871bc0a45dc76c331fc5bcc9a53499d0a
Haydn Dufrene <haydn@mopub.com>
2011-09-07 12:55:49 -0700

    Created the request ivar and now MPAdManager simply changes the URL for each connection, seems to be working... going to run some performance tests. review needed soon

--- MoPubiOS/MoPubSDK/MPAdManager.m
+++ MoPubiOS/MoPubSDK/MPAdManager.m
@@ -63,6 +63,7 @@ NSString * const kAdTypeClear = @"clear";
 @property (nonatomic, retain) NSMutableData *data;
 @property (nonatomic, retain) NSMutableSet *webviewPool;
 @property (nonatomic, retain) MPBaseAdapter *currentAdapter;
+@property (nonatomic, retain) NSMutableURLRequest *request;
 
 - (void)loadAdWithURL:(NSURL *)URL;
 - (void)forceRefreshAd;
@@ -73,7 +74,6 @@ NSString * const kAdTypeClear = @"clear";
 - (NSString *)scaleFactorQueryStringComponent;
 - (NSString *)timeZoneQueryStringComponent;
 - (NSString *)locationQueryStringComponent;
-- (NSURLRequest *)serverRequestObjectForUrl:(NSURL *)URL;
 - (void)replaceCurrentAdapterWithAdapter:(MPBaseAdapter *)newAdapter;
 - (void)scheduleAutorefreshTimerIfEnabled;
 - (void)scheduleAutorefreshTimer;
@@ -113,6 +113,7 @@ NSString * const kAdTypeClear = @"clear";
 @synthesize data = _data;
 @synthesize webviewPool = _webviewPool;
 @synthesize currentAdapter = _currentAdapter;
+@synthesize request = _request;
 
 - (id)initWithAdView:(MPAdView *)adView {
 	if (self = [super init]) {
@@ -124,7 +125,9 @@ NSString * const kAdTypeClear = @"clear";
 		_ignoresAutorefresh = adView.ignoresAutorefresh;
 		_store = [MPStore sharedStore];
 		_timerTarget = [[MPTimerTarget alloc] initWithNotificationName:kTimerNotificationName];
-        _request = [[NSMutableURLRequest alloc] init];
+        _request = [[[NSMutableURLRequest alloc] initWithURL:nil
+                                                 cachePolicy:NSURLRequestUseProtocolCachePolicy 
+                                                 timeoutInterval:kMoPubRequestTimeoutInterval] retain];
         [_request setValue:MPUserAgentString() forHTTPHeaderField:@"User-Agent"];			
 		[[NSNotificationCenter defaultCenter] addObserver:self
 												 selector:@selector(forceRefreshAd)
@@ -227,9 +230,9 @@ NSString * const kAdTypeClear = @"clear";
 	self.URL = (URL) ? URL : [self serverRequestURL];
 	MPLogDebug(@"Ad view (%p) loading ad with MoPub server URL: %@", self.adView, self.URL);
 	
-	NSURLRequest *request = [self serverRequestObjectForUrl:self.URL];
+    _request.URL = self.URL;
 	[_conn release];
-	_conn = [[NSURLConnection connectionWithRequest:request delegate:self] retain];
+	_conn = [[NSURLConnection connectionWithRequest:_request delegate:self] retain];
 	_isLoading = YES;
 	
 	MPLogInfo(@"Ad manager (%p) fired initial ad request.", self);
@@ -291,18 +294,6 @@ NSString * const kAdTypeClear = @"clear";
 	return result;
 }
 
-- (NSURLRequest *)serverRequestObjectForUrl:(NSURL *)URL {
-	NSMutableURLRequest *request = [[[NSMutableURLRequest alloc] 
-									 initWithURL:URL
-									 cachePolicy:NSURLRequestUseProtocolCachePolicy 
-									 timeoutInterval:kMoPubRequestTimeoutInterval] autorelease];
-	
-	// Set the user agent so that we know where the request is coming from (for targeting).
-	[request setValue:MPUserAgentString() forHTTPHeaderField:@"User-Agent"];			
-	
-	return request;
-}
-
 - (NSDictionary *)dictionaryFromQueryString:(NSString *)query
 {
 	NSMutableDictionary *queryDict = [[NSMutableDictionary alloc] initWithCapacity:1];
@@ -319,17 +310,15 @@ NSString * const kAdTypeClear = @"clear";
 
 - (void)trackClick
 {
-	//NSMutableURLRequest *clickURLRequest = [NSMutableURLRequest requestWithURL:self.clickURL];
-    [clickURLRequest setValue:MPUserAgentString() forHTTPHeaderField:@"User-Agent"];
-	[NSURLConnection connectionWithRequest:clickURLRequest delegate:nil];
+    [_request setURL:self.clickURL];
+	[NSURLConnection connectionWithRequest:_request delegate:nil];
 	MPLogDebug(@"Ad view (%p) tracking click %@", self, self.clickURL);
 }
 
 - (void)trackImpression
 {
-	NSMutableURLRequest *impTrackerURLRequest = [NSMutableURLRequest requestWithURL:self.impTrackerURL];
-    [impTrackerURLRequest setValue:MPUserAgentString() forKey:@"User-Agent"];
-	[NSURLConnection connectionWithRequest:impTrackerURLRequest delegate:nil];
+    [_request setURL:self.impTrackerURL];
+	[NSURLConnection connectionWithRequest:_request delegate:nil];
 	MPLogDebug(@"Ad view (%p) tracking impression %@", self, self.impTrackerURL);
 }
 
@@ -667,6 +656,7 @@ NSString * const kAdTypeClear = @"clear";
 
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection
 {
+    
 	// Generate a new webview to contain the HTML and add it to the webview pool.
 	UIWebView *webview = [self adWebViewWithFrame:(CGRect){{0, 0}, self.adView.creativeSize}];
 	webview.delegate = self;
